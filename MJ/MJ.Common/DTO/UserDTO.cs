28f371996ad2254964d2289a207b665a0b2c5c9d
imel <imel@email.com>
2014-07-29 23:31:03 +0800

    added UserDTO.cs

---
+++ MJ/MJ.Common/DTO/UserDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJ.Common.DTO
+{
+    public class UserDto
+    {
+
+        public Guid UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordSalt { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+
+        public DateTime DateDeleted { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
