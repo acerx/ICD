a3a6346086cc82584328ff147a7ea33527e65821
imel <imel@email.com>
2014-07-30 00:40:38 +0800

    added myscripts

---
+++ MJ/MJ.Web.Admin/Scripts/myScripts.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() {
+    
+    //$('#login-username').focus();
+
+    $('#myModal').on('hide', function (e) {
+        //if (!confirm('You want to close me?'))
+        //    e.preventDefault();
+        alert("123");
+    });
+
+});
+
