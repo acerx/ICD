d0d7efe6a5f0cc3e7980650b7ffa7b5eef14e51d
imel <imel@email.com>
2014-07-31 20:50:10 +0800

    added code for saving or adding blog post

--- MJ/MJ.Web.Admin/Controllers/BlogController.cs
+++ MJ/MJ.Web.Admin/Controllers/BlogController.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MJ.Common.DTO;
+using MJ.Services;
+using MJ.Services.IServices;
 using MJ.Web.Admin.Models;
 
 namespace MJ.Web.Admin.Controllers
 {
     public class BlogController : Controller
     {
+
+        #region SERVICES
+
+        private IPostService _postService;
+
+        public IPostService PostService
+        {
+            get { return _postService ?? (_postService = new PostService()); }
+        }
+
+        #endregion
+
         //
         // GET: /Blog/
         [Authorize]
@@ -26,11 +41,51 @@ namespace MJ.Web.Admin.Controllers
         {
             if (!string.IsNullOrEmpty(posts.PostTitle) && !string.IsNullOrEmpty(posts.PostDetails.PostText))
             {
-                TempData["Message"] = "Successfully added new post.";
-                return RedirectToAction("Index", "Blog");
+                PostsDto postsDto = BuildPostDto(posts);
+                bool check = PostService.AddPost(postsDto);
+
+                if (check)
+                {
+                    TempData["Message"] = "Successfully added new post.";
+                    return RedirectToAction("Index", "Blog");
+                }
+
             }
 
             return View();
         }
+
+        #region PRIVATES
+
+        private PostsDto BuildPostDto(PostsModel posts)
+        {
+            var postsDto = new PostsDto
+            {
+                PostId = Guid.NewGuid(),
+                PostTitle = posts.PostTitle,
+                PostDateTime = DateTime.Now,
+                PostDetailsDto = BuildPostDetailsDto(posts.PostDetails),
+                PostDeleted = false,
+                UserId = posts.UserId
+            };
+
+            postsDto.PostDetailsDto.PostId = postsDto.PostId;
+            return postsDto;
+        }
+
+        private PostDetailsDto BuildPostDetailsDto(PostDetails postDetails)
+        {
+            var postDetailsDto = new PostDetailsDto
+            {
+                PostDetailId = Guid.NewGuid(),
+                PostText = postDetails.PostText,
+                PostImage = postDetails.PostImage
+            };
+
+            return postDetailsDto;
+        }
+
+        #endregion
+
     }
 }
