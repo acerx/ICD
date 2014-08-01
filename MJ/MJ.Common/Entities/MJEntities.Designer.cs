f2e84d4e241e5f37b26d92d571225cdf83fed84f
imel <imel@email.com>
2014-08-01 23:55:25 +0800

    revamp entities and database, post title "20" to "150"

--- MJ/MJ.Common/Entities/MJEntities.Designer.cs
+++ MJ/MJ.Common/Entities/MJEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Misc\8. MyRepos\MJL\ICD\MJ\MJ.Common\Entities\MJEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\10. NewProgs\mjl\ICD\MJ\MJ.Common\Entities\MJEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- MJ/MJ.Services/PostService.cs
+++ MJ/MJ.Services/PostService.cs
@@ -31,10 +31,24 @@ namespace MJ.Services
                     result = true;
                 }
             }
-            catch (Exception ex)
+            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
             {
-                
-                throw new Exception(ex.Message);
+
+                Exception exception = dbEx;
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        string message = string.Format("{0}:{1}",
+                            validationErrors.Entry.Entity.ToString(),
+                            validationError.ErrorMessage);
+
+                        //create a new exception inserting the current one
+                        //as the InnerException
+                        exception = new InvalidOperationException(message, exception);
+                    }
+                }
+                throw exception;
             }
 
             return result;
